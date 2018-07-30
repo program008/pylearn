ce331494916e0f83c05c6166901a02abd71f78b8
liutao <tao.liu@uurobot.cn>
2018-07-30 18:03:54 +0800

    Initial commit

---
+++ demo/demo2.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python3
+"""
+  Created by tao.liu on 2018/7/30.
+  Python3 基本数据类型
+"""
+counter = 100  # 整型变量
+miles = 1000.0  # 浮点型变量
+name = "runoob"  # 字符串
+
+print(counter)
+print(miles)
+print(name)
+
+# Python3 的六个标准数据类型中：
+# 不可变数据（3 个）：Number（数字）、String（字符串）、Tuple（元组）；
+# 可变数据（3 个）：List（列表）、Dictionary（字典）、Set（集合）。
+
+# Number类型
+# type()不会认为子类是一种父类类型。
+# isinstance()会认为子类是一种父类类型。
+# 可以通过使用del语句删除单个或多个对象
+
+# 字符串
+# 1、反斜杠可以用来转义，使用r可以让反斜杠不发生转义。
+# 2、字符串可以用+运算符连接在一起，用*运算符重复。
+# 3、Python中的字符串有两种索引方式，从左往右以0开始，从右往左以-1开始。
+# 4、Python中的字符串不能改变。
+
+# List列表
+list = ['abcd', 786, 2.23, 'runoob', 70.2]
+tinylist = [123, 'runoob']
+print("----------List列表----------")
+print(list)  # 输出完整列表
+print(list[0])  # 输出列表第一个元素
+print(list[1:3])  # 从第二个开始输出到第三个元素
+print(list[2:])  # 输出从第三个元素开始的所有元素
+print(tinylist * 2)  # 输出两次列表
+print(list + tinylist)  # 连接列表
+
+# Tuple（元组）
+tuple = ('abcd', 786, 2.23, 'runoob', 70.2)
+tinytuple = (123, 'runoob')
+print("--------Tuple（元组）------------")
+print(tuple)  # 输出完整元组
+print(tuple[0])  # 输出元组的第一个元素
+print(tuple[1:3])  # 输出从第二个元素开始到第三个元素
+print(tuple[2:])  # 输出从第三个元素开始的所有元素
+print(tinytuple * 2)  # 输出两次元组
+print(tuple + tinytuple)  # 连接元组
+
+# Set（集合）
+
+print("--------Set（集合）------------")
+student = {'Tom', 'Jim', 'Mary', 'Tom', 'Jack', 'Rose'}
+
+print(student)  # 输出集合，重复的元素被自动去掉
+
+# 成员测试
+if 'Rose' in student:
+    print('Rose 在集合中')
+else:
+    print('Rose 不在集合中')
+
+# set可以进行集合运算
+a = set('abracadabra')
+b = set('alacazam')
+
+print(a)
+
+print(a - b)  # a和b的差集
+
+print(a | b)  # a和b的并集
+
+print(a & b)  # a和b的交集
+
+print(a ^ b)  # a和b中不同时存在的元素
+
+# Dictionary（字典）
+print("--------Dictionary（字典）------------")
+dict = {}
+dict['one'] = "1 - 菜鸟教程"
+dict[2] = "2 - 菜鸟工具"
+
+tinydict = {'name': 'runoob', 'code': 1, 'site': 'www.runoob.com'}
+
+print(dict['one'])  # 输出键为 'one' 的值
+print(dict[2])  # 输出键为 2 的值
+print(tinydict)  # 输出完整的字典
+print(tinydict.keys())  # 输出所有键
+print(tinydict.values())  # 输出所有值
