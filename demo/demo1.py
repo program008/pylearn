c8a1c128d0768cecd0748a66d792c52a116c723c
liutao <tao.liu@uurobot.cn>
2019-01-18 15:19:00 +0800

    time

--- demo/demo1.py
+++ demo/demo1.py
@@ -1,32 +1,32 @@
 #!/usr/bin/env python3
 
-str='Runoob'
+str = 'Runoob'
 
-print(str)                 # 输出字符串
-print(str[0:-1])           # 输出第一个到倒数第二个的所有字符
-print(str[0])              # 输出字符串第一个字符
-print(str[2:5])            # 输出从第三个开始到第五个的字符
-print(str[2:])             # 输出从第三个开始的后的所有字符
-print(str * 2)             # 输出字符串两次
-print(str + '你好')        # 连接字符串
+print(str)  # 输出字符串
+print(str[0:-1])  # 输出第一个到倒数第二个的所有字符
+print(str[0])  # 输出字符串第一个字符
+print(str[2:5])  # 输出从第三个开始到第五个的字符
+print(str[2:])  # 输出从第三个开始的后的所有字符
+print(str * 2)  # 输出字符串两次
+print(str + '你好')  # 连接字符串
 
 print('------------------------------')
 
-print('hello\nrunoob')      # 使用反斜杠(\)+n转义特殊字符
-print(r'hello\nrunoob')     # 在字符串前面添加一个 r，表示原始字符串，不会发生转义
+print('hello\nrunoob')  # 使用反斜杠(\)+n转义特殊字符
+print(r'hello\nrunoob')  # 在字符串前面添加一个 r，表示原始字符串，不会发生转义
 
-#string = input("\n\n按下 enter 键后退出。")
-#print("输入的内容是"+string)
+# string = input("\n\n按下 enter 键后退出。")
+# print("输入的内容是"+string)
 
 
-x="a"
-y="b"
+x = "a"
+y = "b"
 # 换行输出
-print( x )
-print( y )
+print(x)
+print(y)
 
 print('---------')
 # 不换行输出
-print( x, end=" " )
-print( y, end=" " )
+print(x, end=" ")
+print(y, end=" ")
 print()
